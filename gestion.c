98521d058780baeb2f3626c2c8cd25b1886509f0
salvaH21 <sholivares90@gmail.com>
2023-10-31 11:47:24 +0100

    Inicio de programa y menú

---
+++ gestion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+    printf("Lista de juegos \n");
+    printf("Elige una opcion: \n");
+    printf("\t 1 - Ver lista de juegos \n");
+    printf("\t 2 - Introducir juego \n");
+    printf("\t 3 - Eliminar juego \n");
+    printf("\t 4 - Modificar juego \n");
+    return 0;
+}
