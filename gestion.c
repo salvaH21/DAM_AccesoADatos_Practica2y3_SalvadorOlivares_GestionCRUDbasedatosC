1f61e06f223ba4be97482465db977d9a31d2d3d2
salvaH21 <sholivares90@gmail.com>
2023-10-31 19:02:15 +0100

    nada relevante

--- gestion.c
+++ gestion.c
@@ -2,13 +2,24 @@
 #include <string.h>
 
 int main(int argc,char *argv[]){
+    FILE *archivo;
+    char basededatos[] = "lista";
+    
     //MENU
-    printf("Lista de juegos \n");
+    printf("Con que quieres trabajar? Juegos o peliculas? \n");
+    char nombrearchivo[] = getchar();
+    printf("%s \n",nombrearchivo);
+    /*char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombrearchivo);
+    strcat(ruta,".txt");
+    
     printf("Elige una opcion: \n");
-    printf("\t 1 - Ver lista de juegos \n");
-    printf("\t 2 - Introducir juego \n");
-    printf("\t 3 - Eliminar juego \n");
-    printf("\t 4 - Modificar juego \n");
+    printf("\t 1 - Ver lista \n");
+    printf("\t 2 - Introducir elemento \n");
+    printf("\t 3 - Eliminar elemento \n");
+    printf("\t 4 - Modificar elemento \n");
     printf("Escribe una opcion(1-4): \n");
     char opcion = getchar();
     printf("Has elegido: %c \n",opcion);
@@ -18,17 +29,23 @@ int main(int argc,char *argv[]){
             printf("LISTA \n");
             break;
         case '2':
-            printf("INTRODUCIR JUEGO \n");
+            printf("INTRODUCIR ELEMENTO \n");
+            printf("Introduce el titulo \n");
+            archivo = fopen(ruta,"a");
+            char *titulo = argv[2];
+            fputs(strcat(titulo,"\n"),archivo);
+            fclose(archivo);
+            printf("Titulo introducido \n");
             break;
         case '3':
-            printf("ELIMINAR JUEGO \n");
+            printf("ELIMINAR ELEMENTO \n");
             break;
         case '4':
-            printf("MODIFICAR JUEGO \n");
+            printf("MODIFICAR ELEMENTO \n");
             break;
         default:
             printf("OPCION NO VALIDA \n");
-    }
+    }*/
     printf("FIN \n");
     return 0;
 }
